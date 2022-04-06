98dfd97a8dbaea0d69cc6f28b83d448620db43df
abdularshad06 <abdularshad390@gmail.com>
2022-04-06 12:07:37 +0530

    Update CreateBulkUsers.sh

--- CreateBulkUsers.sh
+++ CreateBulkUsers.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-for i in {1..50}; do  adduser --disabled-password --gecos "" User$i && chpasswd <<<"User$i:Password@123"; done
+# This will create Bulk Users, user1 to user50 and set password to all users Password@123
+for i in {1..50}; do  adduser --disabled-password --gecos "" user$i && chpasswd <<<"user$i:Password@123"; done
